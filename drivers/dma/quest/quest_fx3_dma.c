96f5389b16e9ad398eeb57c27cada6ff41e00fc4
Frank de Vos <frank.de.vos.91@gmail.com>
2018-02-12 14:09:29 +0100

    rework quest cameralink dma module

---
+++ drivers/dma/quest/quest_fx3_dma.c
@@ -0,0 +1,7 @@
+
+#include "quest_dma_core_functions.h"
+
+static struct quest_ qcdma;
+
+
+//quest,qdma-fx3-1.0.0
---
+++ include/linux/dma/quest_dma_core_ioctl.h
@@ -0,0 +1,43 @@
+#ifndef QUEST_DMA_CORE_IOCTL_H_
+#define QUEST_DMA_CORE_IOCTL_H_
+
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
+/************************************************************************************
+*********************************  IOCTL INTERFACE  *********************************
+************************************************************************************/
+
+typedef struct {
+	uint32_t offset;
+	uint32_t value;
+} qdma_reg_arg_t;
+
+typedef struct {
+	uint32_t ch;
+	uint8_t *buf;
+	uint32_t size_bytes;
+} qdma_buf_arg_t;
+
+// Starts read of selected channel, if not started, and returns when data is available
+// or when stop read is called
+#define QDMA_READ _IOR('q', 1, qdma_buf_arg_t *)
+
+// Writes data to selected channel, reading will be stopped and restarted if not
+// already stopped
+#define QDMA_WRITE _IOW('q', 2, qdma_buf_arg_t *)
+
+// Stops the reading of data
+#define QDMA_STOP_READ _IO('q', 3)
+
+// Sets the size of the selected channel
+#define QDMA_SET_CH_SIZE _IOW('q', 4, qdma_buf_arg_t *)
+
+// Drives the reading of buffers (when not using IRQ)
+#define QDMA_DRIVE_READ _IOR('q', 5, uint32_t *)
+
+// Set and get register functions
+#define QDMA_SETREG _IOW('q', 6, qdma_reg_arg_t *)
+#define QDMA_GETREG _IOR('q', 7, qdma_reg_arg_t *)
+
+#endif // QUEST_DMA_CORE_IOCTL_H_
